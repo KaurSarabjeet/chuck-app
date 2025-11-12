66a5d5f6dc6dfd66a0d9d1a3fadc69ab1c4d5c81
Sarabjeet Kaur <sarabjeetkaur330@gmail.com>
2025-11-12 23:39:49 +0100

    Delete integration_test.sh

--- integration_test.sh
+++
@@ -1,7 +0,0 @@
-# Shell script used in setup/deployment of the application.
-#!/usr/bin/env bash
-set -euo pipefail
-docker compose up -d --build proxy app_blue
-sleep 5
-curl -sf http://localhost:8080 | grep "Chuck Norris"
-echo "Integration OK."
